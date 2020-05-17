47dd332ffa51f021b3c3296573846e2ff52b8336
Ilya99999 <ilay963852@mail.ru>
2020-05-17 19:50:19 +0300

    Delete line

--- src/MyProject/Main.java
+++ src/MyProject/Main.java
@@ -172,6 +172,6 @@ public  class Main extends JFrame {
                     (Calendar.MONTH, Calendar.LONG, Locale.ENGLISH)+ " " + calendar.get(Calendar.YEAR));
                   }// Алгоритм для  календаря!
     //    //Тест GIT
-    // Еще тест
+    // Еще тест и еще тест
 }
 
